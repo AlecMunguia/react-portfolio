3f8c7d7382b9f3d8ebf636c4ecb35a74e94483c0
Spinning Plates <alec.munguia@gmail.com>
2019-11-24 16:08:34 -0600

    Updates

---
+++ precache-manifest.9b1a30a0ab30368791b0e11730e6ae15.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "481d85638c337294fca44bf4c9115f8b",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "b039bfd8aec2633643e4",
+    "url": "/react-portfolio/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "16479721abdafdf23bc8",
+    "url": "/react-portfolio/static/js/2.55e1b789.chunk.js"
+  },
+  {
+    "revision": "b039bfd8aec2633643e4",
+    "url": "/react-portfolio/static/js/main.4ef1c5f0.chunk.js"
+  },
+  {
+    "revision": "7b82ede9c0ee1b7e876e",
+    "url": "/react-portfolio/static/js/runtime-main.4d1b885d.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/react-portfolio/static/media/logo.25bf045c.svg"
+  }
+]);
