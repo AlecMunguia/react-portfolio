b932d0c70be08cce8eaf231f1ea84e031ee14f7a
Spinning Plates <alec.munguia@gmail.com>
2019-11-24 19:44:36 -0600

    added page component files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import './components/Navbar';
+import './components/Home';
+import './components/AboutMe';
+import './components/Skills';
+import './components/Work';
+import './components/Contact';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="Main">
+      <Navbar />
+      <Home />
+      <AboutMe />
+      <Skills />
+      <Work />
+      <Contact />
     </div>
   );
 }
