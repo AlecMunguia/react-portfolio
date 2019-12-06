12ef67269d7703e41e7e431e636a4409fda99e6d
Alec Munguia <alec.munguia@gmail.com>
2019-12-06 16:51:10 -0600

    made basic home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,19 @@
 import React from 'react'
 import Navbar from './Navbar'
 import '../App.css'
+import './style/Home.css'
 
 function Home() {
     return(
-        <div>
-            <Navbar />
-            <h1>Home</h1>
-        </div>
+        <div className='Home'>
+        <Navbar />
+            <div className='Text' > 
+                i'm alec munguia
+                    <div className='Subtext'>
+                        a web developer
+                    </div>
+                </div>
+            </div>
     )
 }
 
