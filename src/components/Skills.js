91499f1523aafc4959a399cbaecf3eed4ec8415e
Spinning Plates <alec.munguia@gmail.com>
2019-12-09 16:26:02 -0600

    added skills list

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,11 +1,34 @@
-import React from 'react'
+import React from "react";
+import "./style/Skills.css";
+import "../App.css";
+import Navbar from "./Navbar";
+import './style/Navbar.css'
 
 function Skills() {
-    return(
-        <div>
-            <h1>Skills</h1>
-        </div>
-    )
+  return (
+    <div>
+      <Navbar />
+      <div className = 'Skills'>
+        <h1>Skills</h1>
+        <ul className='SkillsList'>
+          <li>Javascript</li>
+          <li>HTML5</li>
+          <li>CSS3</li>
+          <li>JQuery</li>
+          <li>Responsive UI</li>
+          <li>Node.js</li>
+          <li>React.js</li>
+          <li>Redux</li>
+          <li>Sequelize</li>
+          <li>Express</li>
+          <li>MySQL</li>
+          <li>MongoDB</li>
+          <li>Adobe Photoshop/Illustrator</li>
+          <li>Audio Production</li>
+        </ul>
+      </div>
+    </div>
+  );
 }
 
-export default Skills
+export default Skills;
